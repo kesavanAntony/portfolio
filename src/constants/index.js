dcf3a6d752d899650d735ecaf2a4692773c98a90
kesavan <kesavan338@gmail.com>
2024-10-21 10:24:54 +0530

    third

--- src/constants/index.js
+++ src/constants/index.js
@@ -39,7 +39,7 @@ export const PROJECTS = [
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["HTML","Tailwind", "React",],
-    link:"#"
+    link:"https://kesavanportfolio.netlify.app"
   },
 ];
 
