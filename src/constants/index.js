1c1ab869b7eb3c58d6a8ae36a5fb5faeb7cae438
kesavan <kesavan338@gmail.com>
2025-01-23 15:42:30 +0530

    webdevloper

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,16 +3,16 @@ import project2 from "../assets/projects/razo.png";
 import project3 from "../assets/projects/portfolio.png";
 import project4 from "../assets/projects/imagify.png";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With a years of hands-on experience, I have honed my skills in front-end technologies like React js, as well as back-end technologies like Node.js, Express.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate Web developer with a knack for crafting robust and scalable web applications. With a years of hands-on experience, I have honed my skills in front-end technologies like React js, as well as back-end technologies like Node.js, Express.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 1 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, Express.js, MySQL and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile Web developer with a passion for creating efficient and user-friendly web applications. With 1 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, Express.js, MySQL and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
     year: "Oct 2023 - Present",
-    role: "Full Stack Developer",
+    role: "Web Developer",
     company: "Optimista Training & Development India Pvt Ltd",
-    description: `Lead a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    description: `Developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
     technologies: ["HTML", "CSS", "Bootstrap", "Javascript", "React.js", "NodeJs", "Express", "MongoDB", "Mysql"],
   },
 ];
